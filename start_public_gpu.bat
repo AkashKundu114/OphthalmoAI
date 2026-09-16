1d70244687b73096a4095286e1f07f87db611c39
Akash Kundu <akashkundu1152@gmail.com>
2026-09-16 23:50:01 +0530

    chore: remove legacy root script entrypoints

--- start_public_gpu.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-title OphthalmoAI - Public GPU Launcher
-cd /d "%~dp0"
-echo ===================================================
-echo Starting OphthalmoAI Public GPU Server...
-echo ===================================================
-venv_gpu\Scripts\python.exe -u scripts\start_public_gpu.py
-pause
